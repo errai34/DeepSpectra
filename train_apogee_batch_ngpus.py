20b9daff8ca664a2fcbce3a692b74a0e57fc08a7
Ioana Ciuca <ioana.ciuca034@gmail.com>
2021-04-05 16:41:22 +0300

    apogee batch

--- train_apogee_batch_ngpus.py
+++ train_apogee_batch_ngpus.py
@@ -1,7 +1,6 @@
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
-from time import time
 
 import torch
 import torch.optim as optim
@@ -18,16 +17,17 @@ from torch.distributions import (
 )
 from torch.nn.parameter import Parameter
 
-from nflib_gpu.flows import (
+from nflib.flows import (
     AffineConstantFlow,
     ActNorm,
     Invertible1x1Conv,
     NormalizingFlow,
     NormalizingFlowModel,
 )
-from nflib_gpu.spline_flows import NSF_CL
+from nflib.spline_flows import NSF_CL
 
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+torch.cuda.set_device(device)
 
 if device.type == 'cuda':
     print(torch.cuda.get_device_name(0))
@@ -35,14 +35,14 @@ elif device.type == "cpu":
     print('Using the cpu...')
 
 # choose data here
-spectra = np.loadtxt("./data/apogee_batch_Xtrain.csv")
+spectra = np.loadtxt("./data/jo_batch_spectra.csv")
 
+spectra = spectra[:, 1:]
 spectra = spectra.T
-print(spectra.shape)
 
-#use even number of dimensions
-#lower dimensionality for purpose of testing
-spectra = spectra[:, 1:51]
+#lower the dimensionality for the purpose of testing
+
+spectra = spectra[:, :100]
 spectra = torch.Tensor(spectra)
 dim = spectra.shape[-1]
 print(dim)
@@ -53,60 +53,49 @@ prior = MultivariateNormal(base_mu, base_cov)
 
 # configure the normalising flow
 nfs_flow = NSF_CL
-flows = [nfs_flow(dim=dim, K=8, B=3, hidden_dim=128) for _ in range(5)]
-convs = [Invertible1x1Conv(dim=dim) for _ in flows]
-norms = [ActNorm(dim=dim) for _ in flows]
+flows = [nfs_flow(dim=dim, device=device, K=8, B=3, hidden_dim=128) for _ in range(3)]
+convs = [Invertible1x1Conv(dim=dim, device=device) for _ in flows]
+norms = [ActNorm(dim, device) for _ in flows]
 flows = list(itertools.chain(*zip(norms, convs, flows)))
 
 # initialise the model
-model = NormalizingFlowModel(prior, flows)
+model = NormalizingFlowModel(prior, flows, device)
 
 if torch.cuda.device_count() > 1:
   print("Let's use", torch.cuda.device_count(), "GPUs!")
   model = nn.DataParallel(model)
 
-torch.cuda.set_device(device)
-model.cuda(device)
-
+model = model.to(device)
 # optimizer
 optimizer = optim.Adam(model.parameters(), lr=2e-6, weight_decay=0)  # todo tune WD
 print("number of params: ", sum(p.numel() for p in model.parameters()))
 
 # train_loader
 train_loader = torch.utils.data.DataLoader(
-    spectra, batch_size=100, shuffle=True, pin_memory=True)
-
-t0 = time()
+    spectra, batch_size=71, shuffle=True, pin_memory=True
+)
 
 model.train()
 print("Started training")
-n_epochs = 500
-loss_history=[]
-
-for k in range(n_epochs):
+for k in range(1000):
+    batchidx = 0
     for batch_idx, data_batch in enumerate(train_loader):
-        x = data_batch.cuda(non_blocking=True)
+        x = data_batch.to(device)
         zs, prior_logprob, log_det = model(x)
         logprob = prior_logprob + log_det
-        loss = -torch.mean(logprob).cuda(device)  # NLL
+        loss = -torch.mean(logprob)  # NLL
+        print('done batch', batchidx)
+        batchidx += 1
 
         model.zero_grad()
         loss.backward()
         optimizer.step()
-        loss_history.append(loss.detach().cpu())
 
     if k % 500 == 0:
         print("Loss at step k =", str(k) + ":", loss.item())
 
-t1 = time()
-print(f'Elapsed time: {t1-t0:.1f} s')
-
-elapsed_time = [t1-t0]
-
-path = f"model_apogee_batch_ngpus_test.pth"
+path = f"model_galah_batch_ngpus.pth"
 torch.save(model.state_dict(), path)
-np.savetxt('apogee_batch_loss.npy', loss_history)
-#np.savetxt('apogee_batch_elapsed_time.npy', elapsed_time)
 
 print("Hooray. You're done.")
 print("Saved model to:", path)
